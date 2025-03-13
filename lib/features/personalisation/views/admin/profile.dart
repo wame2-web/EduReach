e27b5acacf8ee85945b428eab21212de8ceadf13
Wangu Phillip <wanguphillip@gmail.com>
2025-03-13 04:03:52 +0200

    Feat: add design

--- lib/features/personalisation/views/admin/profile.dart
+++ lib/features/personalisation/views/admin/profile.dart
@@ -1,3 +1,4 @@
+import 'package:edureach/widgets/profile_input_field.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -8,11 +9,50 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
+      body: Column(
+        children: [
+
+          // User image
+          CircleAvatar(
+            backgroundColor: Colors.black,
+            radius: 50,
+            // backgroundImage: AssetImage("assets/user_avatar.jpeg"),
+          ),
+
+          SizedBox(height: 15,),
+
+          // User Name
+          ProfileInputField(
+            label: "Username",
+            controller: _usernameController,
+            icon: Icons.edit,
+          ),
+
+          SizedBox(height: 16,),
+
+          // User email
+          ProfileInputField(
+            label: "Email",
+            controller: _emailController,
+            icon: Icons.edit,
+          ),
+
+          // User password
+          ProfileInputField(
+            label: "Password",
+            controller: _emailController,
+            icon: Icons.remove_red_eye,
+          ),
+
+
+        ],
       ),
     );
   }
