b6ce4e4a92d1f55c14f197cd6d58c6d7d810f93d
Wangu Phillip <wanguphillip@gmail.com>
2025-03-25 12:39:42 +0200

    Feat: general search

--- lib/features/personalisation/views/admin/dashboard_content.dart
+++ lib/features/personalisation/views/admin/dashboard_content.dart
@@ -1,3 +1,4 @@
+import 'package:edureach/widgets/search_input_text.dart';
 import 'package:flutter/material.dart';
 
 class DashboardContent extends StatefulWidget {
@@ -9,12 +10,30 @@ class DashboardContent extends StatefulWidget {
 
 class _DashboardContentState extends State<DashboardContent> {
 
+  final TextEditingController _searchController = TextEditingController();
+  String _searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard Content"),
+      body: Column(
+        children: [
+
+          const SizedBox(height: 16),
+
+          // Search Input
+          SearchTextField(
+            controller: _searchController,
+            hintText: 'Search courses...',
+            onChanged: (value) {
+              setState(() {
+                _searchQuery = value.toLowerCase();
+              });
+            },
+          ),
+          const SizedBox(height: 16),
+
+        ],
       ),
     );
   }
